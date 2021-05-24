5f463154a673d08a7feb561532411b1d27d4942b
juanysho <juanmonterocallealta@gmail.com>
2021-05-24 15:17:19 +0200

    Home and MySkills Component added

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,12 +2,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { PagesComponent } from './pages.component';
+import { HomeComponent } from './home/home.component';
+import { MyskillsComponent } from './myskills/myskills.component';
 
 const routes: Routes = [
     { 
-        path: 'dashboard', 
+        path: '', 
         component: PagesComponent,
         children: [
+            { path: 'home', component: HomeComponent },
+            { path: 'my-skills', component: MyskillsComponent },
         ]
     },
 ];
